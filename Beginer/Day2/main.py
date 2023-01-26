dbb75c2d3ad92caf51a3dead121bcb186d121e47
Radoslaw Szylkowski <radoslaw.szylkowski2@globallogic.com>
2023-01-26 23:30:19 +0100

    first 3 days projects

---
+++ Beginer/Day2/main.py
@@ -0,0 +1,6 @@
+print("Welcom to the tip calculator.")
+total_bill = float(input("What was the total bill? "))
+number_of_people = int(input("How many people to split the bill? "))
+tip_percentage = int(input("What percentage tip would you like to give? 0, 10, 12, or 15? "))
+pay_per_person = total_bill / number_of_people
+print(f"Each person should pay: {round(pay_per_person + (pay_per_person *(tip_percentage / 100)),2):.2f}")
